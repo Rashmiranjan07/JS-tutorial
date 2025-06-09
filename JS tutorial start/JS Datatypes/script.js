bd03dcf6055959a925cf4b6eb278ce06c183ac1b
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-09 22:47:08 +0530

    added

---
+++ JS tutorial start/JS Datatypes/script.js
@@ -0,0 +1,28 @@
+let age = 4;
+console.log(age);
+
+let price = 102.90;
+
+// string data type
+
+let fullName = "tony starck";
+
+//Bolean data types 
+
+isFollow = true;
+
+//Undefined data types 
+
+let x;
+
+//null data types 
+
+let y = null;
+
+// BigInt Data types
+
+let z = BigInt("123");
+
+//Symabol Dtat types
+
+let r = Symbol("hello");
