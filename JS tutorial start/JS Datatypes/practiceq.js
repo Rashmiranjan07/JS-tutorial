df0b178225d21dde0e8fd2c236ed54e2915a88d1
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-10 23:45:43 +0530

    added

---
+++ JS tutorial start/JS Datatypes/practiceq.js
@@ -0,0 +1,19 @@
+const product = {
+    title: "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+};
+
+console.log(product);
+
+// create a instragram profiles data 
+
+const profile = {
+    username: "rashhh",
+    isFollow: false,
+    followers: 500,
+    following: 200,
+};
+
+console.log(profile);
