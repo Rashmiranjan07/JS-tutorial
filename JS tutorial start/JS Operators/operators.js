b9f14f4968830affea929c6715bdd75e2083c153
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-11 22:14:20 +0530

    added

---
+++ JS tutorial start/JS Operators/operators.js
@@ -0,0 +1,86 @@
+// Arithmetioc operators 
+let a = 5;
+let b = 2;
+
+
+console.log("a =", a, " & b=", b);
+console.log("a+b =", a + b);
+console.log("a-b =", a - b);
+console.log("a*b =", a * b);
+console.log("a/b=", a / b);
+
+//Modulus operator (%)
+console.log("a%b=", a % b);
+
+
+//Exponential operators (power)
+//sign = **
+console.log("a**b=", a ** b); //5^2
+
+
+//Increment operator (++)
+//a++ - a= a + 1
+console.log("a =", a, " & b=", b);
+a++;
+console.log("a=", a);
+
+
+
+//Increment operator (--)
+//a-- - a= a - 1
+console.log("a =", a, " & b=", b);
+a--;
+console.log("a=", a);
+
+
+console.log("a++=", a++);
+console.log("a=", a);
+
+
+console.log("--a=", --a);
+console.log("a=", a);
+
+//Asignemet operators 
+// = , += , -= , *= , %= , **= 
+//a+=1  a=a+1
+//a+=4  a=a+4
+
+let A = 5;
+let B = 2;
+A += 4;
+console.log("A=", A);
+
+A -= 4;
+console.log("A=", A);
+
+A *= 4;
+console.log("A=", A);
+
+A /= 4;
+console.log("A=", A);
+
+A %= 4;
+console.log("A=", A);
+
+
+//Comparison operators 
+//equal to (==)
+
+let x = 5;
+let y = 2;
+console.log("5==2", x == y);
+
+//not equal to (!=)
+console.log("5==2", x != y);
+
+//equal to & type (===)
+//(check the value and data type )
+let X = 5; // number
+let Y = "2"; // string 
+console.log("X===Y", X === Y);
+
+//> , >= , < , <=
+console.log("x>y", x > y);
+console.log("x>=y", x >= y);
+console.log("x<y", x < y);
+console.log("x<=y", x <= y);
