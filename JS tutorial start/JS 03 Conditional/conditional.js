f30a56fa117cd21c818d37d106a017bc65785989
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-13 23:16:34 +0530

    added

--- JS tutorial start/JS 03 Conditional/conditional.js
+++ JS tutorial start/JS 03 Conditional/conditional.js
@@ -74,3 +74,18 @@ if (day === "monday") {
 }
 
 console.log(number);
+
+//switch case
+const expr = "Papayas";
+switch (expr) {
+    case "Oranges":
+        console.log("Oranges are $0.59 a pound.");
+        break;
+    case "Mangoes":
+    case "Papayas":
+        console.log("Mangoes and papayas are $2.79 a pound.");
+        // Expected output: "Mangoes and papayas are $2.79 a pound."
+        break;
+    default:
+        console.log(`Sorry, we are out of ${expr}.`);
+}
