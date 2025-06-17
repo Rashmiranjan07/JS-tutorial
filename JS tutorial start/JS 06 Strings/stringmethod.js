445fcf82e96bc87e63288fbc6ed6484daa7d8091
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-17 21:05:37 +0530

    added

---
+++ JS tutorial start/JS 06 Strings/stringmethod.js
@@ -0,0 +1,6 @@
+//String Method in JS :-
+//These are built-in function to manipulate a string 
+alert("hey");
+
+
+//start from 18/06...
--- JS tutorial start/JS 06 Strings/strings.js
+++ JS tutorial start/JS 06 Strings/strings.js
@@ -1,1 +1,43 @@
-prompt ("hey")
+//string :- 
+//String is a sequence of character used to represent text .
+
+//create string
+let str ="apna college";
+let str2 ='rash';
+
+//String Length
+str.length
+
+//String indicates
+// str[0], str[1], str[2]
+console.log(str[8]);
+
+
+//template literals:-
+//A way to have embedded expressions in strings
+
+let specialString = `This is a template literal ${1+2+3}`;
+console.log(specialString);
+
+//ex:-
+
+let obj ={
+    item :"pen",
+    price: 10,
+};
+console.log("the cost of", obj.item, "is", obj.price, "rupees");
+// the above process is too complicated , we cn easily write this code by using template literal string 
+
+let output = `the cost of ${obj.item} is ${obj.price} rupees `;
+console.log(output);
+
+//string interpulation 
+//to create string by doing substitution of placeholder 
+//  `string text ${expression} string text`
+
+
+//escape character 
+// "\n" - next line 
+console.log("apnna \n college");
+//  "\t" - tab space 
+console.log("apnna \t college");
