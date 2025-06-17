d0dcda2cbc64af9c25106ff03fffed63afa31cbf
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-17 20:14:37 +0530

    added

--- JS tutorial start/JS 05 Loops/loops.js
+++ JS tutorial start/JS 05 Loops/loops.js
@@ -1,29 +1,29 @@
-// //Lopps in js :-
-// //Loops are used to execute a piece of code again and again .
+//Lopps in js :-
+//Loops are used to execute a piece of code again and again .
 
 
-// //01 For loop :-
+//01 For loop :-
 
-//  for (let count=1; count <= 5; count++) {
-//     console.log("apna college");
-//  }
-
-
-
-//  //ex -02 
-//  // (Q) calculate the sum of 1 to 5 numbers .
-
-//  let sum = 0;
-//  for (let i=1; i<=5; i++) {
-//     sum = sum + i;
-//  }
-
-//  console.log("sum=", sum);
+ for (let count=1; count <= 5; count++) {
+    console.log("apna college");
+ }
 
 
-//  for (let i=1; i<=5; i++) { // if we use Var rather than (let) then the last number is 6 .
-//     console.log("i=",i);
-//  }
+
+ //ex -02 
+ // (Q) calculate the sum of 1 to 5 numbers .
+
+ let sum = 0;
+ for (let i=1; i<=5; i++) {
+    sum = sum + i;
+ }
+
+ console.log("sum=", sum);
+
+
+ for (let i=1; i<=5; i++) { // if we use Var rather than (let) then the last number is 6 .
+    console.log("i=",i);
+ }
  
 
 
@@ -31,29 +31,29 @@
 
 
 
-//  //02 Infinite loops :----(never used this bocz its very ...)
-//  //Infinite loop :- A loop that never ends .
+ //02 Infinite loops :----(never used this bocz its very ...)
+ //Infinite loop :- A loop that never ends .
 
-// //  for (let i=1; i>=0; i++) {
-// //     console.log("i=", i);
-// //  }
-
-
-// //03 . While loop :-
-
-// let r = 1;
-// while (r<=5) {
-//     console.log("r=",r);
-//     r++;
-// }
+//  for (let i=1; i>=0; i++) {
+//     console.log("i=", i);
+//  }
 
 
-// //04. do-while loop :-
-//  let z = 20;
-//  do {
-//     console.log("Apna college");
-//     i++;
-//  } while (z<=10);
+//03 . While loop :-
+
+let r = 1;
+while (r<=5) {
+    console.log("r=",r);
+    r++;
+}
+
+
+//04. do-while loop :-
+ let z = 20;
+ do {
+    console.log("Apna college");
+    i++;
+ } while (z<=10);
 
 
 
@@ -85,4 +85,28 @@ for(let k in student) {
 }
 
 
-// practice question (17.06.25)------
+// practice question :- 
+//print all even numbers from 0 to 100 .
+
+
+for (let num = 0; num<=100; num++) {
+    if (num%2===0) {
+        console.log ("num=", num);
+    } 
+}
+
+
+
+
+//practice question 02 :-
+// create a game where you start with any random game number . Ask the user to keep guesssing the game number until the user enters correct value .   
+
+
+
+let gameNum = 25;
+let userNum = prompt("Guess the game number: ");
+while (userNum != gameNum) {
+  userNum = prompt(" you entered the wrong game number. Guess again: ");
+}
+
+console.log("congratulation, you Guess the right number ");
