57621375122a6dfb10eae8ea1317c15e0c1fb5e0
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-18 22:17:11 +0530

    added

--- JS tutorial start/JS 06 Strings/stringmethod.js
+++ JS tutorial start/JS 06 Strings/stringmethod.js
@@ -4,4 +4,49 @@
 
 //str.toUpperCase()
 
-let str = "rashmi ranjan";
+let str = "rashmiRanjan";
+str =str.toUpperCase();
+console.log(str);
+
+
+//str.toUpperCase()
+//as same as upper case 
+
+
+//str.trim()
+// its removes whitespaces from starting and ending .
+
+let ptr = "    Rashmi ranjan Shaw   ";
+console.log(ptr.trim());
+
+
+//str.slice(start,end?)
+//return part of string 
+
+let ntr = "0123456";
+console.log(ntr.slice(1,3));
+
+
+//str1.concat(str2)
+//joins str2 with str1
+
+let rtr1 = " Rashmi";
+let rtr2 = "ranjan";
+console.log(rtr1.concat(rtr2));
+
+
+//str.replace(seacrVal,newval)
+
+let atr = "sonam";
+console.log(atr.replace("s", "p"));
+
+
+//str.charAt(idx)
+
+let btr="IluvJS";
+console.log(btr.charAt(2));
+
+
+
+
+// Practice Question 
