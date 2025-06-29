5fdba083560800e7ade21db3e6f2131b571a5dd6
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-29 23:57:38 +0530

    added

--- JS tutorial start/JS 09 DOM/03-Practice-Qustion/script.js
+++ JS tutorial start/JS 09 DOM/03-Practice-Qustion/script.js
@@ -1,7 +1,14 @@
 // practice question :-
 // Q . Create a H2 headings element with text -"Hello Javascript". Append "from Apna college students" to this text using Js .
 
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + "from Apna college student";
+
+
 
 // Q . Create 3 dives with common name - " box". Access them & add some unique  
+
 
 
