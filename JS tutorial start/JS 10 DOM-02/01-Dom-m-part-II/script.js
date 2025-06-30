cc50fdf67bfc4c42bee9b27a508fedf8ab436166
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-06-30 19:51:08 +0530

    added

--- JS tutorial start/JS 10 DOM-02/01-Dom-m-part-II/script.js
+++ JS tutorial start/JS 10 DOM-02/01-Dom-m-part-II/script.js
@@ -1,93 +1,89 @@
-// // DOM manipulation part - 02
+// DOM manipulation part - 02
 
 
-// // Attributes :--
+// Attributes :--
 
-// // .getAttribute(attr) 
-// //to get the attributes value 
-// let div = document.querySelector("div");
-// console.log(div);
-// let id = div.getAttribute("id");
-// console.log(id);
-// let name = div.getAttribute("name");
-// console.log(name);
-
-
-
-// let para = document.querySelector("p");
-// console.log(para.getAttribute("class"));
-
-
-// //setAttribute(attr, value) 
-// // to set the attributes
-
-// let para = document.querySelector("p");
-// console.log(para.setAttribute("class","newClass"));
-
-
-// //style 
-// //node.style
-// let div = document.querySelector("div");
-
-// div.style.backgroundColor = "green";
-// div.style.backgroundColor = "purple";
-// div.style.visibility = "hidden";
-
-// div.innerText = "hello";
+// .getAttribute(attr) 
+//to get the attributes value 
+let div = document.querySelector("div");
+console.log(div);
+let id = div.getAttribute("id");
+console.log(id);
+let name = div.getAttribute("name");
+console.log(name);
 
 
 
-// //insert elements :-
-
-// // notes :- let el = document.createElement("div")
-
-// let newBtn = document.createElement("button");
-// newBtn.innerText = "click me!"
-// console.log(newBtn);
-
-// //node.append(el)
-// //adds at the end of nodee(inside)
-
-// let div = document.querySelector("div");
-// div.append(newBtn);
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
 
 
-// //node.prepend(el)
-// //adds at the starting of nodes(inside)
-// let div = document.querySelector("div");
-// div.prepend(newBtn);
+//setAttribute(attr, value) 
+// to set the attributes
+
+let para = document.querySelector("p");
+console.log(para.setAttribute("class","newClass"));
 
 
-// //node.before(el)
-// //adds before the node(outside)
-// let div = document.querySelector("div");
-// div.before(newBtn);
+//style 
+//node.style
+let div = document.querySelector("div");
 
+div.style.backgroundColor = "green";
+div.style.backgroundColor = "purple";
+div.style.visibility = "hidden";
 
-// //node.after(el)
-// //adds after the node (outside)
-// let p = document.querySelector("p");
-// p.after(newBtn);
+div.innerText = "hello";
 
 
 
-// let newHeading = document.createElement("h1");
-// newHeading.innerHTML ="<i>hi, I am new!</i>"
+//insert elements :-
 
-// document.querySelector("body").prepend(newHeading);
+// notes :- let el = document.createElement("div")
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!"
+console.log(newBtn);
+
+//node.append(el)
+//adds at the end of nodee(inside)
+
+let div = document.querySelector("div");
+div.append(newBtn);
 
 
-// //delete element :-
+//node.prepend(el)
+//adds at the starting of nodes(inside)
+let div = document.querySelector("div");
+div.prepend(newBtn);
 
-// //node.remove()
-// //remove the nodes 
-// let para = document.querySelector("p");
-// para.remove();
 
-// newHeading.remove();
+//node.before(el)
+//adds before the node(outside)
+let div = document.querySelector("div");
+div.before(newBtn);
 
-// Practice question :-
-//create a new button element. Give it a text "click me", background color of red & text color of white 
 
-//-inssert the button as the first element inside the body tag.
+//node.after(el)
+//adds after the node (outside)
+let p = document.querySelector("p");
+p.after(newBtn);
+
+
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML ="<i>hi, I am new!</i>"
+
+document.querySelector("body").prepend(newHeading);
+
+
+//delete element :-
+
+//node.remove()
+//remove the nodes 
+let para = document.querySelector("p");
+para.remove();
+
+newHeading.remove();
+
 
--- JS tutorial start/JS 10 DOM-02/02-practice-question/script.js
+++ JS tutorial start/JS 10 DOM-02/02-practice-question/script.js
@@ -1,1 +1,18 @@
-prompt("hey")
+// Practice question :-
+//Q.create a new button element. Give it a text "click me", background color of red & text color of white 
+//-inssert the button as the first element inside the body tag.
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "clickme!";
+newBtn.style.color ="white"
+newBtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newBtn);
+
+
+//Q. Create a <p>tag in html, give in a class & some styling .
+// Now create a new class in CSS and try to append this class to the <p> elements.
+/* notes- Did you notice , how you overwrite the class name when you add a new one?
+ solve this problem using classList. */
+
+let para = document.querySelector("p");
