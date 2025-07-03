7fef55771959a13fe3833d86f542e94bccb3d6f5
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-03 20:08:22 +0530

    added

---
+++ JS tutorial start/JS 13 MINIPROJECT/script.js
@@ -0,0 +1,14 @@
+let  userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const choiceId = choice.getAttribute("id");
+        console.log("choice was clicked", choiceId);
+    });
+});
+
+
+//23 min 
