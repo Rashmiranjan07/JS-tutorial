46e5cc951dd3ed54aa265f1d839ea4e681d7ccfc
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2025-07-26 18:59:44 +0530

    added

---
+++ JS tutorial start/JS by SCS/01-Variables/script.js
@@ -0,0 +1,42 @@
+// var, let, const - line by line comparison 
+
+// declaration & initialization 
+//var a; //declare 
+
+//var a=12; //declare and initialize 
+// window mein add karta hai 
+// function scoped hpta hai 
+// aap firse declare kr sakte ho same name and error nahi ayega 
+
+//let a =12;
+
+//const discount = 12;
+// never change the value 
+// it remains constant always as it is .
+
+
+
+// Scope (global,block,functional)
+// var = function scoped 
+// let = block code 
+
+
+// reassignment , redeclaration 
+var a =12;
+a=32;
+
+let b =14;
+b=15;
+
+// we can re-assignment the value of let& var 
+// but we can not redeclare in let . only redeclar occours in var .
+
+
+// Temporal dead zone 
+// can not initailazation before declartion 
+
+// Hoisting impact per type 
+//hoisting -> ek variable ko jav js main banaate hai to wo variable do hiso main toot jata hei and uska declare  part upar chala jatta hei and uska initialization part neeche rehe jata hei .
+// var a= undefined 
+// a=12;
+// hoisting is applicable for let , var , const 
